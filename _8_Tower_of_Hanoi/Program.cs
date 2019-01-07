4fd8aba8f9736f60433d0f97f3880c584dec80ee
Albert Stjärne <albert.stjarne@gmail.com>
2019-01-07 15:05:10 +0100

    setting no of discs and sourcePeg, destinationPeg and sparePeg, condition for when recursion should stop.

---
+++ _8_Tower_of_Hanoi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _8_Tower_of_Hanoi
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+        static void Tower(int n, int sourcePeg, int destinationPeg, int sparePeg)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine(sourcePeg + "->" + destinationPeg);
+            }
+            else
+            {
+                Tower(n-1)
+            }
+        }
+
+    }
+
+}
